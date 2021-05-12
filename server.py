8a13e59b502db23bd22065c07976193033fb88e1
ShawnAli97 <shawn10ali@gmail.com>
2021-05-12 16:57:59 -0400

    Sign in and out works

--- server.py
+++ server.py
@@ -6,19 +6,32 @@ class server:
         
     def greeting(self):
         print("welcome ",self.server_name) 
+    
+    def sign_out_message(self):
+        print("Signing out " , self.server_name)
 
     def sign_in(self):
-        option = input("Do you want to sign in? yes or no?")
 
+        option = input("Do you want to sign in? yes or no?")
         if option == "no":
             sys.exit()
         else:
             d.greeting()
 
+    def sign_out(self):
+        option = input("Do you want to sign out? yes or no?")
 
+        if option == "no":
+            sys.exit()
+        else:
+            d.sign_out_message()
+
+
+#Testing server function
 d = server()
 d.get_server_name()
 d.sign_in()
+d.sign_out()
 
 
 
