78038c98aad55b7b15622a5ed5f3eceab7050927
ShawnAli97 <shawn10ali@gmail.com>
2021-05-15 03:28:54 -0400

    Created ordering class

--- server.py
+++ server.py
@@ -33,6 +33,11 @@ class server:
 
     def order(self):
         customer = input("what do you want to order? Enter 1 for a taco. Enter 2 for a burrito.")
+        try:
+            customer = int(customer)
+        except:
+            print("Only numbers")
+            sys.exit()
         if customer == 1:
             print("You ordered a taco")
             self.tacos
